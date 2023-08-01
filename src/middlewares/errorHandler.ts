fa4ba6518b6804213201ffb5fe0214640638e463
IvanRafael-Dev <ivanrafael.dev@gmail.com>
2023-07-31 22:56:13 -0300

    feat: add errorHandler

---
+++ src/middlewares/errorHandler.ts
@@ -0,0 +1,10 @@
+import { ErrorRequestHandler } from "express";
+
+const errorHandler: ErrorRequestHandler = 
+  (err, req, res, next) =>
+    res
+      .status(err.statusCode || 500)
+      .json({ message: err.message });
+
+
+export { errorHandler };
